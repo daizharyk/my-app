dd8115138e5993495861def51517de30b3a7e529
Didar <aizharyk78@gmail.com>
2025-03-28 20:22:20 +0500

    1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -25,6 +25,11 @@ const Header = () => {
       }
     };
 
+    useEffect(() => {
+      setMenuOpen(false);
+    }, [location.pathname]);
+
+    
     document.addEventListener("click", handleClickOutside);
     return () => {
       document.removeEventListener("click", handleClickOutside);
